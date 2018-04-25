1607593aa8e9a9c4c02b4fbdb2a8433e7ab50f96
4da11in <dallin.h907@stu.nebo.edu>
2018-04-25 14:19:36 -0600

    here
    commit

--- portfolioSite/script.js
+++ portfolioSite/script.js
@@ -1,25 +1,31 @@
-var mb = document.getElementsByClassName("menuBackground");
+
 var i = 0;
+var subPages = document.getElementsByClassName("subPage");
+var menus = document.getElementsByClassName("menu");
 function menuClicked(index) {
+    for (var j = 0; j < 4; j++) {
+        subPages[i].style["display"] = "none";
+    }
     i = index;
-    switch (index) {
-        case 0:
-            break;
-                 }
+    subPages[i].style["display"] = "block";
+    //alert(menus[i].offsetLeft);
 }
-var offsets = [-275, -175, 20, 153];
+//var offsets = [-275, -175, 20, 153];
+
 function onresize() {    
     var screenWidth = document.documentElement.clientWidth;    
-    document.getElementById('menuBackground').style.color = "red";//screenWidth/2 + offsets[i] + "px";
-    alert("left");
+    document.getElementById('menuBackground').style.left = screenWidth/2 + offsets[i] + "px";
+    
 }
 
 $(".menu").click(function() {
     var a = $(this);
     var screenWidth = document.documentElement.clientWidth;
-    $(".menuBackground").animate( {
-        left: screenWidth/2 + offsets[i] + "px",
+    
+    $("#menuBackground").animate( {
+        left: menus[i].offsetLeft,//screenWidth/2 + offsets[i] + "px",
         width: a.width()+20
-    });
+    }, 300);
     
 });
+
